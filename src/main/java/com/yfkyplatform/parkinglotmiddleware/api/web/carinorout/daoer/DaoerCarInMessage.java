5db3e7eff99510f56d50b5e7e582df864ff8d7d3
suhuyuan <tiger5331819@163.com>
2022-06-20 18:51:10 +0800

    更新入场出场记录时间格式

--- src/main/java/com/yfkyplatform/parkinglotmiddleware/api/web/carinorout/daoer/DaoerCarInMessage.java
+++ src/main/java/com/yfkyplatform/parkinglotmiddleware/api/web/carinorout/daoer/DaoerCarInMessage.java
@@ -1,10 +1,7 @@
 package com.yfkyplatform.parkinglotmiddleware.api.web.carinorout.daoer;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 /**
  * 车辆入场记录通知
@@ -19,11 +16,11 @@ public class DaoerCarInMessage {
     private String parkingNo;
     @ApiModelProperty(value = "车牌",required = true)
     private String carNo;
-    @ApiModelProperty(value = "卡类型",required = true)
+    @ApiModelProperty(value = "卡类型", required = true)
     private int cardTypeId;
-    @ApiModelProperty(value = "入场时间",required = true)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd HH:mm:ss.SS")
-    private LocalDateTime inTime;
-    @ApiModelProperty(value = "入场图片地址",required = true)
+    @ApiModelProperty(value = "入场时间", required = true)
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd HH:mm:ss.SS")
+    private String inTime;
+    @ApiModelProperty(value = "入场图片地址", required = true)
     private String inPic;
 }
--- src/main/java/com/yfkyplatform/parkinglotmiddleware/api/web/carinorout/daoer/DaoerCarOutMessage.java
+++ src/main/java/com/yfkyplatform/parkinglotmiddleware/api/web/carinorout/daoer/DaoerCarOutMessage.java
@@ -1,10 +1,7 @@
 package com.yfkyplatform.parkinglotmiddleware.api.web.carinorout.daoer;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 /**
  * 车辆入场记录通知
@@ -19,11 +16,11 @@ public class DaoerCarOutMessage {
     private String parkingNo;
     @ApiModelProperty(value = "车牌",required = true)
     private String carNo;
-    @ApiModelProperty(value = "卡类型",required = true)
+    @ApiModelProperty(value = "卡类型", required = true)
     private int cardTypeId;
-    @ApiModelProperty(value = "出场时间",required = true)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd HH:mm:ss.SS")
-    private LocalDateTime outTime;
-    @ApiModelProperty(value = "出场图片地址",required = true)
+    @ApiModelProperty(value = "出场时间", required = true)
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd HH:mm:ss.SS")
+    private String outTime;
+    @ApiModelProperty(value = "出场图片地址", required = true)
     private String outPic;
 }
