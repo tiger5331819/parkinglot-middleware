05e84c12501c85ba7dc2a34d7d713bfa5e40f857
suhuyuan <tiger5331819@163.com>
2022-08-30 11:00:03 +0800

    增加文档

--- src/main/java/com/yfkyplatform/parkinglotmiddleware/universal/middleware/TaskChainBuilder.java
+++ src/main/java/com/yfkyplatform/parkinglotmiddleware/universal/middleware/TaskChainBuilder.java
@@ -3,7 +3,9 @@ package com.yfkyplatform.parkinglotmiddleware.universal.middleware;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * 中间件建造器
@@ -12,7 +14,7 @@ import java.util.function.Consumer;
  */
 
 public class TaskChainBuilder<TData> {
-    private final List<Consumer<TData>> middlewareList;
+    private final List<Function<Consumer<TData>, Consumer<TData>>> middlewareList;
     private final Consumer<TData> completeTask;
 
     public TaskChainBuilder() {
@@ -26,16 +28,30 @@ public class TaskChainBuilder<TData> {
         this.completeTask = worker;
     }
 
-    public TaskChainBuilder<TData> use(Consumer<TData> chain) {
+    public TaskChainBuilder<TData> use(Function<Consumer<TData>, Consumer<TData>> chain) {
         this.middlewareList.add(chain);
+        return this;
+    }
+
+    public TaskChainBuilder<TData> use(BiConsumer<TData, Consumer<TData>> chain) {
+        use((next) -> (data) -> chain.accept(data, next));
+        return this;
+    }
+
+    public TaskChainBuilder<TData> use(Consumer<TData> chain) {
+        use((next) -> (data) -> {
+            chain.accept(data);
+            next.accept(data);
+        });
         return this;
     }
 
     public void work(TData data) {
         Consumer<TData> request = completeTask;
-        Collections.reverse(middlewareList);
-        for (Consumer<TData> middleware : middlewareList) {
-            request = middleware.andThen(request);
+        List<Function<Consumer<TData>, Consumer<TData>>> workList = new ArrayList<>(middlewareList);
+        Collections.reverse(workList);
+        for (Function<Consumer<TData>, Consumer<TData>> middleware : workList) {
+            request = middleware.apply(request);
         }
         request.accept(data);
     }
--- src/test/java/com/yfkyplatform/parkinglotmiddleware/universal/TaskChainTest.java
+++ src/test/java/com/yfkyplatform/parkinglotmiddleware/universal/TaskChainTest.java
@@ -1,5 +1,6 @@
 package com.yfkyplatform.parkinglotmiddleware.universal;
 
+import cn.hutool.core.util.StrUtil;
 import com.yfkyplatform.parkinglotmiddleware.universal.middleware.TaskChainBuilder;
 import lombok.Data;
 import org.junit.jupiter.api.Test;
@@ -13,24 +14,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class TaskChainTest {
 
     @Test
-    void test() {
-        TaskChainBuilder<TestData> taskChain = new TaskChainBuilder<>((data) -> {
-            System.out.println(data);
-        });
+    void testNew() {
+        TaskChainBuilder<TestData> taskChain = new TaskChainBuilder<>((data) -> System.out.println(data));
         taskChain.use((testData) -> {
             testData.setValue(2);
             System.out.println("task1 value:" + testData.getValue());
-        }).use((testData) -> {
-            testData.setValue(3);
-            System.out.println("task1 value:" + testData.getValue());
+        }).use((testData, next) -> {
+            if (StrUtil.equals(testData.getId(), "No")) {
+                next.accept(testData);
+            } else {
+                testData.setValue(3);
+                System.out.println("task2 value:" + testData.getValue());
+                next.accept(testData);
+            }
         }).use((testData) -> {
             testData.setValue(4);
-            System.out.println("task1 value:" + testData.getValue());
+            System.out.println("task3 value:" + testData.getValue());
         });
         TestData data = new TestData();
         data.setId("TaskChainTest");
         data.setValue(0);
+        System.out.println("TaskChainTest run");
         taskChain.work(data);
+
+        TestData data2 = new TestData();
+        data2.setId("No");
+        data2.setValue(0);
+        System.out.println("No run");
+        taskChain.work(data2);
     }
 
     @Data
