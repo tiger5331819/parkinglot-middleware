eb22bafb97e7ef57caedfaa9e5a4cea7d8abdf7b
suhuyuan <tiger5331819@163.com>
2023-10-24 17:55:51 +0800

    主动释放netty缓存

--- src/main/java/com/yfkyplatform/parkinglotmiddleware/universal/web/ParkingLotConnectException.java
+++ src/main/java/com/yfkyplatform/parkinglotmiddleware/universal/web/ParkingLotConnectException.java
@@ -8,9 +8,6 @@ package com.yfkyplatform.parkinglotmiddleware.universal.web;
 
 public class ParkingLotConnectException extends RuntimeException{
     private static final String MESSAGE ="远端车场链接异常";
-    ParkingLotConnectException() {
-        super(MESSAGE);
-    }
 
     public ParkingLotConnectException(Throwable cause) {
         super(MESSAGE,cause);
